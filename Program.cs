c309dc5f8f0ce9cf9c288124bf4db786d99500cc
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-08-29 20:38:47 -0300

    Primeiro exercicio de programação

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Preparar...");
+Thread.Sleep(1500);
+Console.WriteLine("Apontar...");
+Thread.Sleep(1500);
+Console.WriteLine("Fogo!");
+Thread.Sleep(1500);
+Console.WriteLine("Presione uma tecla para continuar...");
+Console.ReadKey();
